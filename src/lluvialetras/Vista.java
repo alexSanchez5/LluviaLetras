ecfde49bfa52a92dc9bc4c0bde2652e817a84dfe
Arkhleo <34568470+Arkhleo@users.noreply.github.com>
2018-01-29 08:51:36 +0100

    Crear menu

--- src/lluvialetras/Vista.java
+++ src/lluvialetras/Vista.java
@@ -5,15 +5,23 @@
  */
 package lluvialetras;
 
+import java.awt.Menu;
+import java.awt.MenuBar;
+import java.awt.MenuItem;
+import javax.swing.JFrame;
+
 /**
  *
  * @author alejandro sanchez
  */
-public class Vista {
+public class Vista extends JFrame{
     
     private Controlador c;
     private Barra barra;
     private Letra letra;
+    private MenuBar menu;
+    private Menu archivo;
+    private Menu nivel;
     
     public Vista(Controlador c){
         this.c=c;
@@ -23,8 +31,29 @@ public class Vista {
         barra=new Barra();
     }
     
+    /**
+     * Crea el menu
+     */
     public void crearMenu(){
+        menu=new MenuBar();
         
+        archivo=new Menu("Archivo");
+        MenuItem guardar=new MenuItem("Guardar");
+        archivo.add(guardar);
+        MenuItem cargar=new MenuItem("Cargar");
+        archivo.add(cargar);
+        MenuItem salir=new MenuItem("Salir");
+        archivo.add(salir);
+       
+        nivel=new Menu("Nivel");
+        for (int i = 1; i < 6; i++) {
+            MenuItem nNivel=new MenuItem("Nivel "+i);
+            nivel.add(nNivel);
+        }
+        
+        menu.add(archivo);
+        menu.add(nivel);
+        this.setMenuBar(menu);
     }
     
     public void crearVentana(){
